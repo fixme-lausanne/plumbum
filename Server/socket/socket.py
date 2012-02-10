94e0add2ded9c3ad04db387ac857620386985c29
gcmalloc <gcmalloc@gmail.com>
2012-02-11 00:26:54 +0100

    adding socket.py metaprogramming fun :p

--- Server/socket/socket.py
+++ Server/socket/socket.py
@@ -1,18 +1,40 @@
-import socket 
+from socket import socket
 
-Class SocketServer(Process):
-    def __init__(self, post_port=1338, get_port=1339):
+Class SocketServerManager(Process):
+
+    def __init__(self, post_port=1338, get_port=1339, host=None):
+        self = Process.__init__(self)
+        self.servers = []
+        self.servers += socket_server_factory(host, post_port, self.get_handler)
+        self.servers += socket_server_factory(host, get_port, self.post_handler)
+        self.servers.append(post_serv)
         
-        
-    def run(self, )
-        #open the two socket and listen to them
-        post_socket = socket.create_connexion()
-        get_socket = socket
-        while 1:
-            
-            
+    
+    @staticmethod
+    def socket_server_factory(host, port, callback):
+        s = socket.getaddrinfo(host, port, family=0, socktype=0, proto=0, flags=0)
+        return SocketServer(callback, s)
+
+    def run(self):
+        for i in self.servers:
+            i.start()
+
     def post_handler():
         #handle the post request
         
+
     def get_handler():
         #handle the get request
+        
+class SocketServer(SocketServer):
+    SEM_MAX=30
+    def __init__(self, callback_method, skt):
+        self = Process.__init__(self)
+        self.skt = skt
+        self.sem = Semaphore(SocketServer.SEM_MAX)
+        
+    def run(self):
+        s.listen()
+        while 1:
+            s.listen(1)
+            
