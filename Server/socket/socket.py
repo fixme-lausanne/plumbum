dff17f83c5670b5b30a87cdf55cf1d590707131a
gcmalloc <gcmalloc@gmail.com>
2012-02-11 00:32:06 +0100

    correcting syntax

--- Server/socket/socket.py
+++ Server/socket/socket.py
@@ -5,11 +5,10 @@ Class SocketServerManager(Process):
     def __init__(self, post_port=1338, get_port=1339, host=None):
         self = Process.__init__(self)
         self.servers = []
-        self.servers += socket_server_factory(host, post_port, self.get_handler)
-        self.servers += socket_server_factory(host, get_port, self.post_handler)
+        self.servers.append(socket_server_factory(host, post_port, self.get_handler))
+        self.servers.append(socket_server_factory(host, get_port, self.post_handler))
         self.servers.append(post_serv)
-        
-    
+
     @staticmethod
     def socket_server_factory(host, port, callback):
         s = socket.getaddrinfo(host, port, family=0, socktype=0, proto=0, flags=0)
@@ -21,20 +20,20 @@ Class SocketServerManager(Process):
 
     def post_handler():
         #handle the post request
-        
 
     def get_handler():
         #handle the get request
-        
+
 class SocketServer(SocketServer):
-    SEM_MAX=30
+    SEM_MAX = 30
+
     def __init__(self, callback_method, skt):
         self = Process.__init__(self)
         self.skt = skt
         self.sem = Semaphore(SocketServer.SEM_MAX)
-        
+
     def run(self):
         s.listen()
         while 1:
-            s.listen(1)
-            
+            addr = s.listen(1)
+            s.callback()
