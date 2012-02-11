1a7c16dfa0b45dc4d21e3ecc0af49d32ffeb2c4e
Rorist <aubort.jeanbaptiste@gmail.com>
2012-02-11 03:13:22 +0100

    Post is working

--- httpserver/server.py
+++ httpserver/server.py
@@ -5,29 +5,40 @@ from pprint import pprint
 import re
 
 class RequestHandler(BaseHTTPRequestHandler):
+
+
     def do_POST(self):
-        print('received POST: '+self.path)
+        header = open('templates/header.tpl', 'r').read()
+        footer = open('templates/footer.tpl', 'r').read()
+        length = int(self.headers.get('content-length'))
+        post = self.rfile.read(length)
+        print('post this to the db %s'%post)
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(bytes(header+"Data received"+footer, 'utf-8'))
 
     def do_GET(self):
+        header = open('templates/header.tpl', 'r').read()
+        footer = open('templates/footer.tpl', 'r').read()
         reg = re.compile('\/u\/([a-zA-Z0-9]+)')
         m = reg.match(self.path)
         # Show paste
         if m != None:
-            print('got id: %s'%m.group(1))
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write(b'Something from the database')
-            return
-        # Add paste
-        else:
-            header = open('templates/header.tpl', 'r').read()
-            data   = open('templates/form.tpl', 'r').read()
-            footer = open('templates/footer.tpl', 'r').read()
+            paste = 'Stuff from the database'
+            data   = open('templates/show.tpl', 'r').read().replace('{{data}}', paste)
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(header + data + footer)
+            self.wfile.write(bytes(header+data+footer, 'utf-8'))
+            return
+        # Add paste
+        else:
+            data   = open('templates/form.tpl', 'r').read()
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes(header+data+footer, 'utf-8'))
         
 
 httpd = HTTPServer(('', 8000), RequestHandler)
