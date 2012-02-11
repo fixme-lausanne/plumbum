4ad540e76b197794b18c105dfe36cdbfc373e5ef
Valentin Haenel <valentin.haenel@gmx.de>
2012-02-11 04:34:08 +0100

    adding plumbum, needs flask, 2.6 and kyoto 2.6

---
+++ plumbum.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+import db_kyoto as dbk
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return "Welcome to plumbum."
+
+@app.route('/<uid>', methods=['GET'])
+def show_entry(uid):
+    code = dbk.retrieve(uid)
+    lexer = get_lexer_by_name("python", stripall=True)
+    formatter = HtmlFormatter(full=True, style='colorful')
+    return highlight(code, lexer, formatter)
+
+@app.route('/', methods=['POST'])
+def add_entry():
+    code = request.form["lead"]
+    uid = dbk.post(code)
+    return "http://127.0.0.1:5000/%s\n" % uid
+
+if __name__ == "__main__":
+    dbk.init()
+    app.run()
+    dbk.close()
