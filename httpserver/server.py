a9673423b2a38f7c5446748d0863d36d5ba2e37e
Rorist <aubort.jeanbaptiste@gmail.com>
2012-02-11 02:16:36 +0100

    Use templates for form

--- httpserver/server.py
+++ httpserver/server.py
@@ -3,6 +3,7 @@
 from http.server import *
 from pprint import pprint
 import re
+from ..pastebinlib import db_memory
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -11,12 +12,23 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         reg = re.compile('\/u\/([a-zA-Z0-9]+)')
         m = reg.match(self.path)
+        # Show paste
         if m != None:
             print('got id: %s'%m.group(1))
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'asdasd')
+            self.wfile.write(b'Something from the database')
+            return
+        # Add paste
+        else:
+            header = open('templates/header.tpl', 'r').read()
+            data   = open('templates/form.tpl', 'r').read()
+            footer = open('templates/footer.tpl', 'r').read()
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(header + data + footer)
         
 
 httpd = HTTPServer(('', 8000), RequestHandler)
