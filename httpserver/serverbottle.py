e454580082e46ef4c7bed5657e38f02b57535118
gcmalloc <gcmalloc@gmail.com>
2012-02-11 21:16:26 +0100

    Commenting the bottle server, adding a raw, setting the coloration by default

--- httpserver/serverbottle.py
+++ httpserver/serverbottle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Plumbum http server based on bottle"""
 # Add the parent path of this file to pythonpath, so we can import pastebinlib
 from os.path import dirname, abspath
 import sys
@@ -11,9 +12,9 @@ sys.path.append(dirname(dirname(abspath(__file__))))
 from pastebinlib.api import NonExistentUID
 try:
     import pastebinlib.db_kyoto as db
-except:
+except ImportError:
     logging.error('Cannot import kyoto db, falling back to memory db \
-(reason for failure: %s)' % sys.exc_info()[0])
+(reason for failure: {})'.format(sys.exc_info()[0]))
     import pastebinlib.db_memory as db
 from bottle import route, run, request, abort, template, HTTPResponse
 
@@ -38,7 +39,8 @@ def post():
 
 
 @route('/:uid/raw', method='GET')
-def retrieve(uid):
+def raw_retrieve(uid):
+    """Fetch a pastebin entry without coloration"""
     try:
         return db.retrieve(uid)
     except NonExistentUID:
@@ -46,7 +48,8 @@ def retrieve(uid):
 
 
 @route('/:uid', method='GET')
-def retrieve_color(uid):
+def retrieve(uid):
+    """Fetch a pastebin entry with coloration using Pygments lib"""
     try:
         raw_paste = db.retrieve(uid)
         return highlight(raw_paste, PythonLexer(), HtmlFormatter())
