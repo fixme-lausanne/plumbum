3c5c6e8f01a2ed780f406931d3599738de67f24b
Dan Kalotay <d.kalotay@gmail.com>
2012-02-11 04:25:06 +0100

    unused arg removal
    make uid doesn't use preferred uid argument

--- pastebinlib/utils.py
+++ pastebinlib/utils.py
@@ -2,7 +2,7 @@ import hashlib
 import random
 import string
 
-def make_uid(utf8_text, expiry_policy, timeout, prefered_uid, timestamp):
+def make_uid(utf8_text, expiry_policy, timeout, timestamp):
     spec = ''.join((utf8_text, str(expiry_policy), str(timeout), str(timestamp)))
     return hashlib.sha1(spec.encode('utf-8')).hexdigest()
 
