4c64bfe46defb204303a9badac15238c5af21d55
Dan Kalotay <d.kalotay@gmail.com>
2012-02-14 20:21:36 +0100

    Increase sha1 slice size and adjust refine_uid:
    refine_uid now returns a shorter string without uppercase letters.
    If the uid is part of the URL, there is no case sensititvityi, so only one
    case is needed. generate_uid returns more digits of the sha1 digest.
    Within an order of magnitude, there is the same number of possibilites for
    the return values of generate_uid and refine_uid

--- database/utils.py
+++ database/utils.py
@@ -5,9 +5,9 @@ import string
 
 def make_uid(utf8_text, expiry_policy, timestamp):
     spec = ''.join((utf8_text, str(expiry_policy), str(timestamp)))
-    return hashlib.sha1(spec.encode('utf-8')).hexdigest()[:8]
+    return hashlib.sha1(spec.encode('utf-8')).hexdigest()[:10]
 
 
 def refine_uid():
-    return ''.join([random.choice(string.ascii_letters + string.digits) for _
-        in range(16)])
+    return ''.join([random.choice(string.ascii_lowercase + string.digits) for _
+        in range(8)])
