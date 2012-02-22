486c90b12e643dcd4cf6659ffa41b813317f23f8
Dan Kalotay <d.kalotay@gmail.com>
2012-02-22 23:53:10 +0100

    moving things around
    epic refactor

--- database/db.py
+++ database/db.py
@@ -19,6 +19,23 @@ class DataBase(object):
     def write(utf8_content, preferred_uid=None):
         pass
 
+    @staticmethod
+    def _check_expiry(entry):
+        timestamp = datetime.date.fromtimestamp(float(entry['timestamp']))
+        now = datetime.date.fromtimestamp(time.time())
+        policy = entry['expiry_policy']
+        if policy == api.EXPIRY_HOUR_FROM_WRITE:
+            return now - timestamp < datetime.timedelta(hours = 1)
+        elif policy == api.EXPIRY_HOUR_FROM_READ:
+            return now - timestamp < datetime.timedelta(hours = 1)
+        elif policy == api.EXPIRY_WEEK_FROM_WRITE:
+            return now - timestamp < datetime.timedelta(weeks = 1)
+        elif policy == api.EXPIRY_WEEK_FROM_READ:
+            return now - timestamp < datetime.timedelta(weeks = 1)
+        else:
+            return False
+
+
 class NonExistentUID(Exception):
     def __init__(self, uid):
         self.uid = uid
