681387f610aca1e11fed0c312f85c403c43d7a1b
gcmalloc <gcmalloc@gmail.com>
2012-02-24 14:29:42 +0100

    all test can be directly executed

--- unittest/test_db_memory.py
+++ unittest/test_db_memory.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from os.path import dirname, abspath, join
 import sys
 sys.path.append(join(dirname(dirname(abspath(__file__))), "database"))
--- unittest/test_http_server.py
+++ unittest/test_http_server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from os.path import dirname, abspath
 import sys
 sys.path.append(dirname(dirname(abspath(__file__))))
