6912cfacba7a392fded2f46c8442edf9b0b0bb88
gcmalloc <gcmalloc@gmail.com>
2012-02-29 20:31:15 +0100

    moving file around

---
+++ unittest/test_api.py
@@ -0,0 +1,17 @@
+import unittest 
+
+class ApiTest(uniitest.TestCase):
+    
+    def SetUp(self):
+    
+    #def post(utf8_text, expiry_policy=EXPIRY_NEVER, prefered_uid=None,linked_uid_list=None):
+    def test_empty_post(self):
+        
+    def test_wrong_expiry_policy(self):
+        
+    def test_preferred_uid(self):
+        
+    def test_preferred_uid_clash(self):
+        
+    def TearDown(self):
+    
