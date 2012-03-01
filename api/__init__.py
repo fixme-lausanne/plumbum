ab637c71734fb302d69df61a7c3cdf34b57cafb6
gcmalloc <gcmalloc@gmail.com>
2012-03-01 11:10:39 +0100

    name clash

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,9 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from os.path import dirname, abspath
+print(sys.path)
+sys.path.append(dirname(abspath(__file__)))
+#test of module choice should be done there
 
-import api 
-post = api.post
-retrieve = api.retrieve
-NonExistentUID = api.NonExistentUID
+import api
+#from db_memory import MemoryDB as __module__
--- plumbum.py
+++ plumbum.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import rawserver.rawserver as raw
 import sys
-from os.path import join, dirname
-sys.path.append(join(dirname(__file__), "httpserver"))
+from os.path import join, dirname, abspath
+sys.path.append(join(dirname(abspath(__file__)), "httpserver"))
 import bottleserver as http
 
 if __name__ == "__main__":
